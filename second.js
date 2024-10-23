c7cb42dd821b5d909088b16f5cc7d301d2b82184
Shehrozkhannn <sharozekkhan@gmail.com>
2024-10-23 15:05:03 +0500

    Practice two sum another way

--- second.js
+++ second.js
@@ -1,16 +1,28 @@
 
-function twoSum(nums, target) {
-  const map = {};
+// function twoSum(nums, target) {
+//   const map = {};
 
-  for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
-    if (map[complement] !== undefined) {
-      return [map[complement], i];
+//   for (let i = 0; i < nums.length; i++) {
+//     const complement = target - nums[i];
+//     if (map[complement] !== undefined) {
+//       return [map[complement], i];
+//     }
+//     map[nums[i]] = i;
+//   }
+//   return [];
+// }
+
+//SECOND WAY
+
+const twoSum = (nums, target) => {
+  return nums.map((num, i) => {
+    const complementIndex = nums.find((n, j) => j > i && n === target - num);
+
+    if (complementIndex !== undefined) {
+      return [i, nums.indexOf(complementIndex)];
     }
-    map[nums[i]] = i;
-  }
-  return [];
-}
+  }).filter(Boolean)[0];  // Remove undefined entries and return the first match
+};
 
 // Example usage:
 const nums = [2, 7, 11, 15];
