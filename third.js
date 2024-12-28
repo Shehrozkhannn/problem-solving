322fe3c20fe9af8b870439c4876eb60b46f9dc79
Shehrozkhannn <sharozekkhan@gmail.com>
2024-12-29 03:28:35 +0500

    Solve numbers palindrome checking problem

--- third.js
+++ third.js
@@ -14,4 +14,23 @@ const palindrome = (str) => {
        return 'its a palindrome'
 }
 
-console.log(palindrome('civic'))
+// console.log(palindrome('civic'));
+
+
+// PALINDROME PROBLEM FOR NUMBERS!!!
+
+const numbersPalindrome = (num) => {
+    const originalNum = num;
+    let reversedNum = 0;
+    let temp = num;
+
+    while (temp > 0){
+        const digit = temp % 10;
+        reversedNum = reversedNum * 10 + digit;
+        temp = Math.floor(temp / 10);
+    }
+    if(reversedNum === originalNum) return "Its a palindrome"
+    return "Its not a palidrome"
+}
+
+console.log(numbersPalindrome(1000))
