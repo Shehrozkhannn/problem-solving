6e1cd06ef248590b7b1ca15fa99996c49b352799
Shehrozkhannn <sharozekkhan@gmail.com>
2024-12-30 15:15:12 +0500

    Clear code

--- third.js
+++ third.js
@@ -1,36 +1,36 @@
 // PALINDROME PROBLEM WITH OUT CREATING OR CONVERTING THE STRING IN TO ARRAY
 
 
-const palindrome = (str) => {
-    const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-    let start = 0;
-    let end = cleanStr.length - 1 
+// const palindrome = (str) => {
+//     const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+//     let start = 0;
+//     let end = cleanStr.length - 1 
 
-    while(end > start){
-        if(cleanStr[start] !== cleanStr[end]) return  'Its not a palidrome';
-        start++
-        end--
-    }
-       return 'its a palindrome'
-}
+//     while(end > start){
+//         if(cleanStr[start] !== cleanStr[end]) return  'Its not a palidrome';
+//         start++
+//         end--
+//     }
+//        return 'its a palindrome'
+// }
 
-// console.log(palindrome('civic'));
+// // console.log(palindrome('civic'));
 
 
-// PALINDROME PROBLEM FOR NUMBERS!!!
+// // PALINDROME PROBLEM FOR NUMBERS!!!
 
-const numbersPalindrome = (num) => {
-    const originalNum = num;
-    let reversedNum = 0;
-    let temp = num;
+// const numbersPalindrome = (num) => {
+//     const originalNum = num;
+//     let reversedNum = 0;
+//     let temp = num;
 
-    while (temp > 0){
-        const digit = temp % 10;
-        reversedNum = reversedNum * 10 + digit;
-        temp = Math.floor(temp / 10);
-    }
-    if(reversedNum === originalNum) return "Its a palindrome"
-    return "Its not a palidrome"
-}
+//     while (temp > 0){
+//         const digit = temp % 10;
+//         reversedNum = reversedNum * 10 + digit;
+//         temp = Math.floor(temp / 10);
+//     }
+//     if(reversedNum === originalNum) return "Its a palindrome"
+//     return "Its not a palidrome"
+// }
 
-console.log(numbersPalindrome(1000))
+// console.log(numbersPalindrome(1000))
