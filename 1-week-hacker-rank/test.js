8720e8df50af0eccfee2f1432a2ff3bb329d5f1b
Shehrozkhannn <sharozekkhan@gmail.com>
2025-01-01 23:54:04 +0500

    Solve min max problem of hacker rank

--- 1-week-hacker-rank/test.js
+++ 1-week-hacker-rank/test.js
@@ -1,21 +1,34 @@
-function plusMinus(arr) {
-    let sumPositive = 0;
-    let sumNegative = 0;
-    let sumZero = 0;
-    arr.forEach(num => {
-        if(num > 0){
-            sumPositive = sumPositive + 1
-        }else if (num < 0){
-            sumNegative = sumNegative + 1
-        }else{
-            sumZero = sumZero + 1
-        }
-    });
-    let finalPositiveVal = (sumPositive/arr.length).toFixed(6);
-    let finalNegativeVal = (sumNegative/arr.length).toFixed(6);
-    let finalZerosVal = (sumZero/arr.length).toFixed(6);
-    return [finalPositiveVal, finalNegativeVal, finalZerosVal].join('\n')
+// function plusMinus(arr) {
+//     let sumPositive = 0;
+//     let sumNegative = 0;
+//     let sumZero = 0;
+//     arr.forEach(num => {
+//         if(num > 0){
+//             sumPositive = sumPositive + 1
+//         }else if (num < 0){
+//             sumNegative = sumNegative + 1
+//         }else{
+//             sumZero = sumZero + 1
+//         }
+//     });
+//     let finalPositiveVal = (sumPositive/arr.length).toFixed(6);
+//     let finalNegativeVal = (sumNegative/arr.length).toFixed(6);
+//     let finalZerosVal = (sumZero/arr.length).toFixed(6);
+//     return [finalPositiveVal, finalNegativeVal, finalZerosVal].join('\n')
 
+// }
+
+// console.log(plusMinus([-4, 3, -9, 0, 4, 1]));
+
+
+////////////////////////////// MINIMAX PROBLEM ///////////////////////////////////////////////////////////
+
+function miniMaxSum(arr) {
+    const sortedArray = arr.sort((a,b)=> a-b);
+    const minSum = sortedArray.slice(0,arr.length-1).reduce((acc,val)=> acc + val,0);
+    const maxSum = sortedArray.slice(1,arr.length).reduce((acc,val)=> acc + val,0);
+    console.log(minSum, maxSum)
 }
 
-console.log(plusMinus([-4, 3, -9, 0, 4, 1]));
+miniMaxSum([1,3,5,7,9])
+
