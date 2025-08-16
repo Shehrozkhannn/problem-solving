a3a713a029f1843532f6b24acc2a7fefc84cbea9
Shehrozkhannn <sharozekkhan@gmail.com>
2025-08-16 21:52:00 +0500

    Solve foursum leetcode

--- 1-week-hacker-rank/test.js
+++ 1-week-hacker-rank/test.js
@@ -84,7 +84,7 @@ function threeSum(nums) {
     const result = [];
 
     for (let i = 0; i < nums.length - 2; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue; // Skip duplicate first elements
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
 
         let left = i + 1;
         let right = nums.length - 1;
@@ -105,6 +105,44 @@ function threeSum(nums) {
                 left++; // Need a bigger sum
             } else {
                 right--; // Need a smaller sum
+            }
+        }
+    }
+    return result;
+}
+
+// -------------------------- FOUR SUM ------------------------------------------------ 
+
+function fourSum(nums, target) {
+    nums.sort((a, b) => a - b);
+    const result = [];
+    const n = nums.length;
+
+    for (let i = 0; i < n - 3; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        for (let j = i + 1; j < n - 2; j++) {
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+
+            let left = j + 1;
+            let right = n - 1;
+
+            while (left < right) {
+                const sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+                if (sum === target) {
+                    result.push([nums[i], nums[j], nums[left], nums[right]]);
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] === nums[left - 1]) left++;
+                    while (left < right && nums[right] === nums[right + 1]) right--;
+
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
             }
         }
     }
