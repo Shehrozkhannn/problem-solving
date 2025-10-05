f3c3c01f59fd033c594098a33cfc02a2a4513abf
Shehrozkhannn <sharozekkhan@gmail.com>
2025-10-05 17:05:34 +0500

    Solve leetcode problem of merged sorted array

---
+++ Interview-prep-leetcode/prep.js
@@ -0,0 +1,16 @@
+// ------------------------------ MERGE SORTED ARRAY   ----------------------------- 
+
+var merge = function (nums1, m, nums2, n) {
+    let newArr = [...nums1.slice(0, m), ...nums2];
+
+    // Sort them all together
+    newArr.sort((a, b) => a - b);
+
+    // Copy the result back into nums1
+    for (let i = 0; i < newArr.length; i++) {
+        nums1[i] = newArr[i];
+    }
+};
+
+
+let test = merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
