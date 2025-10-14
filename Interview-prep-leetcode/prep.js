477e01fc1319ff78149faa821ec4149ffd3ee19b
Shehrozkhannn <sharozekkhan@gmail.com>
2025-10-14 23:54:25 +0500

    Resolve remove duplicates problem from sorted array in-place method

--- Interview-prep-leetcode/prep.js
+++ Interview-prep-leetcode/prep.js
@@ -20,16 +20,36 @@
 /////////// remove element ///
 
 
-var removeElements = (nums,val) =>{
-  let k = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if(nums[i] !== val){
-      nums[k] = nums[i]
-      k++
+// var removeElements = (nums,val) =>{
+//   let k = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if(nums[i] !== val){
+//       nums[k] = nums[i]
+//       k++
+//     }
+//   }
+//   return k
+
+// }
+
+// console.log(removeElements([3,2,2,3],3))
+
+
+// Remove duplicates from sorted array
+
+
+var removeDuplicates = function(nums) {
+  if (nums.length === 0) return 0;
+
+  let k = 1; // pointer for the next unique position
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[k] = nums[i];
+      k++;
     }
   }
-  return k
 
-}
+  return k;
+};
 
-console.log(removeElements([3,2,2,3],3))
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
